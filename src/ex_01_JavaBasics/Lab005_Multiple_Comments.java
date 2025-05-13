619e632ec54a0f13ee6b7fe6d2b962bfc37d7841
nmanjula <manjuroyal1218@gmail.com>
2025-05-13 18:23:48 +0530

    new updates

---
+++ src/ex_01_JavaBasics/Lab005_Multiple_Comments.java
@@ -0,0 +1,12 @@
+package ex_01_JavaBasics;
+
+public class Lab005_Multiple_Comments {
+    public static void main(String[] args) {
+        /*
+        This are comments
+        for explanation
+        for annatottions
+        manjula n **/
+        System.out.println("with multiple comments");
+    }
+}
