ef2a65e8518002d871b2ba17cd58923e53b2c5bb
nmanjula <manjuroyal1218@gmail.com>
2025-05-13 15:37:54 +0530

    updates

--- src/ex_01_JavaBasics/HelloWorld.java
+++ src/ex_01_JavaBasics/HelloWorld.java
@@ -3,6 +3,11 @@ package ex_01_JavaBasics;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        //This practice program
+        /*
+        * this code will not execute bcz we mentioned comments
+        *
+        * */
 
 
     }
