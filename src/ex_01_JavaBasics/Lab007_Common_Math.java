6cf61feb2e6b1320d272f0ac341cd318b78d79ed
nmanjula <manjuroyal1218@gmail.com>
2025-05-14 15:21:34 +0530

    new updates

---
+++ src/ex_01_JavaBasics/Lab007_Common_Math.java
@@ -0,0 +1,19 @@
+package ex_01_JavaBasics;
+
+public class Lab007_Common_Math {
+    public static void main(String[] args)
+    {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(3/2);// println print gives interger value but not decimal value
+        System.out.println("3/2");
+
+
+
+
+
+
+    }
+}
---
+++ src/ex_01_JavaBasics/Lab009_Multiple_Main.java
@@ -0,0 +1,20 @@
+package ex_01_JavaBasics;
+
+public class Lab009_Multiple_Main {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+
+    }
+
+    public static void main(String args) {
+        System.out.println("Bye");
+
+    }
+
+    public static void main(int[] args) {
+        System.out.println("Hello");
+
+    }
+}
+
+
