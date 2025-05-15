e90226fa9de28feb7ff4a148d40eb3698575e069
nmanjula <manjuroyal1218@gmail.com>
2025-05-15 20:18:01 +0530

    print_15/05/2025

---
+++ src/ex_02_JavaBasics_Part2/Lab014_Printf.java
@@ -0,0 +1,56 @@
+package ex_02_JavaBasics_Part2;
+
+public class Lab014_Printf {
+    public static void main(String[] args) {
+        int a=5;
+        //int b=1;
+        //int c ;
+        System.out.printf("%d*1=%d",a,a*1);
+        System.out.println();
+        System.out.printf("%d*2=%d",a,a*2);
+        System.out.println();
+        System.out.printf("%d*3=%d",a,a*3);
+        System.out.println();
+        System.out.printf("%d*4=%d",a,a*4);
+        System.out.println();
+        System.out.printf("%d*5=%d",a,a*5);
+        System.out.println();
+        System.out.printf("%d*6=%d",a,a*6);
+        System.out.println();
+        System.out.printf("%d*7=%d",a,a*7);
+        System.out.println();
+        System.out.printf("%d*8=%d",a,a*8);
+        System.out.println();
+        System.out.printf("%d*9=%d",a,a*9);
+        System.out.println();
+        System.out.printf("%d*10=%d",a,a*10);
+        //System.out.println();
+        /*
+        System.out.printf("%d",c= a*b);
+        System.out.println();
+        //b=3;
+        System.out.printf("%d", c= a*b);
+        System.out.println();
+        //b=4;
+        System.out.printf("%d",c= a*b);
+        System.out.println();
+        //b=5;
+        System.out.printf("%d",c=a*b);
+        System.out.println();
+        //b=6;
+        System.out.printf("%d",c=a*b);
+        System.out.println();
+        //b=7;
+        System.out.printf("%d",c=a*b);
+        System.out.println();
+        //b=8;
+        System.out.printf("%d",c=a*b);
+        System.out.println();
+        //b=9;
+        System.out.printf("%d",c=a*b);
+        System.out.println();
+        //b=10;
+        System.out.printf("%d",c=a*b);*/
+
+    }
+}
