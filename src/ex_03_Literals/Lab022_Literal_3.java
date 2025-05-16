aa2d716d8ae8a6073fcdcfd9ac74301cfc77d743
nmanjula <manjuroyal1218@gmail.com>
2025-05-16 15:16:37 +0530

    print_16/05/2025_Literals

---
+++ src/ex_03_Literals/Lab022_Literal_3.java
@@ -0,0 +1,10 @@
+package ex_03_Literals;
+
+public class Lab022_Literal_3 {
+    public static void main(String[] args) {
+        {
+            int Enum =20;
+            System.out.println(Enum);
+        }
+    }
+}
---
+++ src/ex_03_Literals/Lab023_Literals_4.java
@@ -0,0 +1,10 @@
+package ex_03_Literals;
+
+public class Lab023_Literals_4 {
+    public static void main(String[] args) {
+        int a=10;
+        int binary_num = 0b110;
+        System.out.println(binary_num);
+    }
+}
+
