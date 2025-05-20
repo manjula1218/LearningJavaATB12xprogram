a2032e57ffc86d5fb2cbcd7ca9dc4be25e272c2f
nmanjula <manjuroyal1218@gmail.com>
2025-05-20 15:09:46 +0530

    print_20/05/2025_Literals

---
+++ src/ex_04_Operators/Lab034_Equals.java
@@ -0,0 +1,9 @@
+package ex_04_Operators;
+
+public class Lab034_Equals {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);//assig operator
+        System.out.println(20==20);//comparision operator
+    }
+}
