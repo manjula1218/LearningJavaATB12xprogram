2ed237874489cce610891c59af5a40d98ac00cf7
nmanjula <manjuroyal1218@gmail.com>
2025-05-23 15:06:06 +0530

    print_23/05/2025_If condition

--- src/ex_02_Practice.java
+++
@@ -1,5 +0,0 @@
-public class ex_02_Practice {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/ex_08_If_Condition/Lab070_Onlyif.java
@@ -0,0 +1,10 @@
+package ex_08_If_Condition;
+
+public class Lab070_Onlyif {
+    public static void main(String[] args) {
+        int a=19;
+        if (a>18){
+            System.out.println("you can vote");
+        }
+    }
+}
