4751856a2e9121de3a95958f4e2d9b18b261c7a0
nmanjula <manjuroyal1218@gmail.com>
2025-05-30 18:00:04 +0530

    print_30/05/2025_switch

---
+++ src/ex_09_Switch/Lab080_Interview2_Switch.java
@@ -0,0 +1,16 @@
+package ex_09_Switch;
+
+public class Lab080_Interview2_Switch {
+    public static void main(String[] args) {
+         char c = 'A';
+         switch (c)
+         {
+             case 65:
+                 System.out.println("Match ASCII");
+                 break;
+             default:
+                 System.out.println("No match");
+         }
+
+    }
+}
---
+++ src/ex_09_Switch/Lab083_Switch_Interview5.java
@@ -0,0 +1,16 @@
+package ex_09_Switch;
+
+public class Lab083_Switch_Interview5 {
+    public static void main(String[] args) {
+        int a= 90;
+        switch (a)
+        {
+            case 90:
+                System.out.println("90");
+                break;
+            case 91:
+                System.out.println("91");
+                break;
+        }
+    }
+}
